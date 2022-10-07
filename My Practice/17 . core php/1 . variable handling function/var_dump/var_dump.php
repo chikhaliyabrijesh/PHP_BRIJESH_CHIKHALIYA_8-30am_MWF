6f5a3b0d8bc5ac76be8794daf7dcff233c4ff265
chikhaliyabrijesh <109718201+chikhaliyabrijesh@users.noreply.github.com>
2022-10-07 15:26:37 +0200

    Add files via upload

---
+++ My Practice/17 . core php/1 . variable handling function/var_dump/var_dump.php
@@ -0,0 +1,13 @@
+<?php
+
+$a=10;
+$b=12.3456;
+$name="Brijesh";
+$name1="Akshar";
+
+echo var_dump($a)."<br>";
+echo var_dump($b)."<br>";
+echo var_dump($name)."<br>";
+echo var_dump($name1)."<br>";
+
+?>
