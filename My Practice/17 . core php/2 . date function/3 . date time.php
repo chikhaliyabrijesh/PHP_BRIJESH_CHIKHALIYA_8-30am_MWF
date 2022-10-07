d5d3faa126fcb9316ff7d88b039eee3ab9ae549b
chikhaliyabrijesh <109718201+chikhaliyabrijesh@users.noreply.github.com>
2022-10-07 15:33:43 +0200

    Add files via upload

---
+++ My Practice/17 . core php/2 . date function/3 . date time.php
@@ -0,0 +1,9 @@
+<?php
+
+date_default_timezone_set("asia/calcutta");
+// date_default_timezone_set("asia/dhaka");
+
+echo "The Time Is : ".date("D/m/Y h:i:s a");
+// echo "The Time Is : ".date("d/m/Y h:i:s a");
+
+?>
---
+++ My Practice/17 . core php/2 . date function/4 . time.php
@@ -0,0 +1,7 @@
+<?php
+
+echo "time is its now : ".date("H:i:s a")."<br>";
+echo "time is its now : ".date("h:i:s a")."<br>";
+
+
+?>
