c476a83cf1b9f6c844e04fa9583f3e8b37453a4e
chikhaliyabrijesh <109718201+chikhaliyabrijesh@users.noreply.github.com>
2022-10-07 15:33:00 +0200

    Add files via upload

---
+++ My Practice/17 . core php/2 . date function/1 . date.php
@@ -0,0 +1,21 @@
+<?php
+
+$today1=date('d/m/y');
+$today2=date('D/M/Y');
+
+$today3=date('d.m.y');
+$today4=date('D.M.Y');
+
+$today5=date('d-m-y');
+$today6=date('D-M-Y');
+
+echo "today date is : ".$today1."<br>";
+echo "today date is : ".$today2."<br>";
+
+echo "today date is : ".$today3."<br>";
+echo "today date is : ".$today4."<br>";
+
+echo "today date is : ".$today5."<br>";
+echo "today date is : ".$today6."<br>";
+
+?>
---
+++ My Practice/17 . core php/2 . date function/2 . date.php
@@ -0,0 +1,8 @@
+<?php
+
+echo "today running month is : ".date("F")."<br><br>";
+echo "today is : ".date("D")."<br><br>";
+echo "today running year is : ".date("Y")."<br><br>";
+
+
+?>
