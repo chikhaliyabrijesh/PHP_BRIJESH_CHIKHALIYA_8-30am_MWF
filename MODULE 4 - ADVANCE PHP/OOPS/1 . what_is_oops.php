5279df36c74f34deee03ce3690ff3c78e3177f5f
chikhaliyabrijesh <109718201+chikhaliyabrijesh@users.noreply.github.com>
2022-11-19 09:13:53 +0530

    Add files via upload

---
+++ MODULE 4 - ADVANCE PHP/OOPS/1 . what_is_oops.php
@@ -0,0 +1,34 @@
+<?php
+
+/*
+
+What is OOPs ?
+
+oops stands for object oriented programming structured language.
+
+oops used to MVC concept in php8.
+
+oops support some features 
+a)	Class
+b)	Object
+c)	Inheritance
+d)	Polymorphism
+e)	Abstract
+f)	Encapsulation
+
+Oops used to crate a dynamic web application or website in PHP8 using MVC architecture.
+Oops provides security to secured your applications 
+Oops used some magic methods 
+Ex: __construct, __destruct etc
+Oops used to protect your data using access modifier and access specifier 
+a)	Public 
+b)	Private 
+c)	Protected 
+
+
+
+
+*/
+
+
+?>
