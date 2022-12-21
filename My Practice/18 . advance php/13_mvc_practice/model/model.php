f23faf68f35e714b589b354c6b3111ec521dac90
chikhaliyabrijesh <109718201+chikhaliyabrijesh@users.noreply.github.com>
2022-12-21 18:24:23 +0530

    Add files via upload

---
+++ My Practice/18 . advance php/13_mvc_practice/model/model.php
@@ -0,0 +1,44 @@
+<?php
+
+class model
+{
+    public $connection;
+    public function __construct()
+    {
+        try
+        {
+            $this->connection=new mysqli("localhost","root","","crud_operation");
+            // echo "connection successfully";
+        }
+        catch(Exception $e)
+        {
+            // die(mysqli_error($this->connection,$e));
+            echo "connection not stablished";
+        }
+    }
+    public function insertdata($table,$data)
+    {
+        $column=array_keys($data);
+        $column1=implode(",",$column);
+
+        $values=array_values($data);
+        $values1=implode("','",$values);
+
+        $insert="insert into $table($column1) values('$values1')";
+        $query=mysqli_query($this->connection,$insert);
+        return $query;
+    }
+
+    public function selectalldata($table)
+    {
+        $select="select * from $table";
+        $execute=mysqli_query($this->connection,$select);
+        while($fetch=mysqli_fetch_array($execute))
+        {
+            $arr[]=$fetch;
+        }
+        return $arr;
+    }
+} 
+
+?>
