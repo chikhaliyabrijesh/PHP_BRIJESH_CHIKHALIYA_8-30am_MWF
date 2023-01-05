77b73e1e8cdd36920ef97bed2937b49006cf6259
chikhaliyabrijesh <109718201+chikhaliyabrijesh@users.noreply.github.com>
2023-01-05 18:25:04 +0530

    Add files via upload

---
+++ My Practice/18 . advance php/14_mvc_practice/changepassword.php
@@ -0,0 +1,27 @@
+
+<!-- breadcrumbs -->
+<div class="breadcrumbs">
+		<div class="container">
+			<ol class="breadcrumb breadcrumb1 animated wow slideInLeft" data-wow-delay=".5s">
+				<li><a href="<?php echo $mainurl;?>"><span class="glyphicon glyphicon-home" aria-hidden="true"></span>Home</a></li>
+				<li class="active">Change Password</li>
+			</ol>
+		</div>
+	</div>
+<!-- //breadcrumbs -->
+<!-- login -->
+	<div class="login">
+		<div class="container">
+			<h2>Change Password ?</h2>
+		
+			<div class="login-form-grids animated wow slideInUp" data-wow-delay=".5s">
+				<form method="post">
+					<input type="password" name="opass" placeholder="Old Password" required=" " >
+					<input type="password" name="npass" placeholder="New Password" required=" " >
+					<input type="password" name="cpass" placeholder="Confirm Password" required=" " >
+					<input type="submit" name="chng" value="Change Password">
+				</form>
+			</div>
+		</div>
+	</div>
+<!-- //login -->
